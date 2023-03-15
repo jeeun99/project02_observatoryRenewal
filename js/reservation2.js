20ad6fb120da7119706e88f1684a3afdbed7e28a
jeeun99 <wpdms27@gmail.com>
2023-03-15 17:26:09 +0900

    2023-03-15 commit

--- js/reservation2.js
+++ js/reservation2.js
@@ -14,3 +14,28 @@ $chooseItem.forEach((item) => {
     }
   });
 });
+
+const $perorteam = document.querySelectorAll('input[name="perOrteam"]');
+const $per = document.querySelector(".person");
+const $team = document.querySelector(".team");
+
+const $per_btn = document.querySelector(".per_btn");
+const $team_btn = document.querySelector(".team_btn");
+
+// console.log($submitBtn);
+
+$perorteam.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+    $per.classList.remove("on");
+    $per_btn.classList.remove("on");
+    $team.classList.remove("on");
+    $team_btn.classList.remove("on");
+    if (i == 0) {
+      $per.classList.add("on");
+      $per_btn.classList.add("on");
+    } else if (i == 1) {
+      $team.classList.add("on");
+      $team_btn.classList.add("on");
+    }
+  });
+});
